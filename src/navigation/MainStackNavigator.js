5ee3b0dc9b8c6582037a2578a49492835e8cf417
Adam Tyrlik <tyrlik.adam@gmail.com>
2020-11-15 03:49:04 -0500

    its late

--- src/navigation/MainStackNavigator.js
+++ src/navigation/MainStackNavigator.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import start from '../screens/start';
+import home from '../screens/home';
 
 const Stack = createStackNavigator();
 
@@ -10,7 +10,7 @@ function MainStackNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="start" component={start} />
+        <Stack.Screen name="home" component={home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
