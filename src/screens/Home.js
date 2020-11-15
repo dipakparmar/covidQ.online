00c7220424fedb292f41055fb6022ed341dad6c0
Dipak Parmar <hi@dipak.tech>
2020-11-14 16:11:45 -0800

    Fixed home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
-function Home() {
+export default function Home() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
